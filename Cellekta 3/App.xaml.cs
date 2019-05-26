6c0e288926a3f5b57a6b15623ae041d27965bc1f
Eli Brighton <eli.brighton@gmail.com>
2019-05-26 10:26:37 +1000

    Setup MVVM architecture

--- Cellekta 3/App.xaml.cs
+++ Cellekta 3/App.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Cellekta_3.Model;
+using Cellekta_3.ViewModel;
+using Cellekta_3.View;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Unity;
 
 namespace Cellekta_3
 {
@@ -13,5 +17,15 @@ namespace Cellekta_3
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            IUnityContainer container = new UnityContainer();
+            container.RegisterType<ISongListViewModel, SongListViewModel>();
+            container.RegisterType<ISongListModel, SongListModel>();
+
+            var window = container.Resolve<SongListView>();
+            window.Show();
+        }
     }
 }
---
+++ Cellekta 3/Base/ObservableObject.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Cellekta_3.Base
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
