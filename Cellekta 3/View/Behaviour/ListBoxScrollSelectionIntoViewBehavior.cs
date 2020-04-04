01a95672543ba7b482a08393a43739f7e31d6a21
Eli Brighton <eli.brighton@gmail.com>
2020-04-04 11:34:57 +1000

    Add scroll to view behaviour to Listbox

---
+++ Cellekta 3/View/Behaviour/ListBoxScrollSelectionIntoViewBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using System.Windows.Threading;
+
+namespace Cellekta_3.View.Behavior
+{
+    public class ListBoxScrollSelecionIntoViewBehavior : Behavior<ListBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.SelectionChanged += AssociatedObject_SelectionChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.SelectionChanged -= AssociatedObject_SelectionChanged;
+        }
+
+        private static void AssociatedObject_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var listBox = sender as ListBox;
+
+            if (listBox == null)
+            {
+                return;
+            }
+
+            Action action = () =>
+            {
+                var selectedItem = listBox.SelectedItem;
+
+                if (selectedItem != null)
+                {
+                    listBox.ScrollIntoView(selectedItem);
+                }
+            };
+
+            listBox.Dispatcher.BeginInvoke(action, DispatcherPriority.ContextIdle);
+        }
+    }
+}
